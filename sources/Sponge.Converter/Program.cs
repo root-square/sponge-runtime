6771cf321d16f3a5194771d866048d04e5ad6a49
June <junimiso04@naver.com>
2023-07-30 00:32:00 +0900

    feat: set-up initial project layout

---
+++ sources/Sponge.Converter/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Sponge.Converter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+
+            static IHostBuilder CreateHostBuilder(string[] args) =>
+                Host.CreateDefaultBuilder(args)
+                    .ConfigureServices((hostContext, services) =>
+                    {
+                        services.AddLogging();
+                        services.AddTransient<IExampleService, ExampleService>();
+                        services.AddHostedService<HostedService>();
+                    })
+                    .ConfigureAppConfiguration((hostingContext, config) =>
+                    {
+                        var env = hostingContext.HostingEnvironment;
+
+                        config.AddEnvironmentVariables();
+                        config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+                        config.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: false, reloadOnChange: true);
+                    });
+        }
+    }
+}
---
+++ sources/Sponge.Wrapper/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Sponge.Wrapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
