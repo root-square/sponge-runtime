20163e0a9079694320e501adf7c1e33b77055d99
June <junimiso04@naver.com>
2023-08-01 21:04:59 +0900

    feat: update project layout

---
+++ sources/Sponge.Bootstrapper/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Sponge.Bootstrapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
--- sources/Sponge.Processor/Program.cs
+++ sources/Sponge.Processor/Program.cs
@@ -1,10 +1,10 @@
-﻿namespace Sponge.Wrapper
+﻿namespace Sponge.Processor
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
