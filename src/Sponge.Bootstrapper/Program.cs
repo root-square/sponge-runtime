c779e98360832970a385763c329c0a066145bb15
June <junimiso04@naver.com>
2023-08-03 20:28:32 +0900

    feat: fix projects

--- src/Sponge.Bootstrapper/Program.cs
+++ src/Sponge.Bootstrapper/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace Sponge.Bootstrapper
+﻿using System.Reflection;
+
+namespace Sponge.Bootstrapper
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
         }
     }
 }
