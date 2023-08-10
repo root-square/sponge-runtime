f14574c0f082dd4b0b6038e9772db2400c3ade72
June <junimiso04@naver.com>
2023-08-11 00:12:01 +0900

    feat: impl console window util, change target framework

--- src/Sponge.Bootstrapper/Utilities/ConsoleHelper.cs
+++ src/Sponge.Bootstrapper/Utilities/ConsoleHelper.cs
@@ -1,12 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Sponge.Bootstrapper.Utilities
 {
-    internal class ConsoleHelper
+    internal static class ConsoleHelper
     {
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetConsoleWindow();
+
+        [DllImport("User32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int cmdShow);
+
+        internal static bool HideWindow()
+        {
+            IntPtr hWnd = GetConsoleWindow();
+
+            if (hWnd != IntPtr.Zero)
+            {
+                ShowWindow(hWnd, 0);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        internal static bool ShowWindow()
+        {
+            IntPtr hWnd = GetConsoleWindow();
+
+            if (hWnd != IntPtr.Zero)
+            {
+                ShowWindow(hWnd, 1);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
