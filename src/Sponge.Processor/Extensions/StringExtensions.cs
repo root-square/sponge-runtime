4d9b72318ba413a9dd3fc2f27e53fa178d63b8c2
June <junimiso04@naver.com>
2023-08-13 01:17:30 +0900

    feat: add string extensions

---
+++ src/Sponge.Processor/Extensions/StringExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sponge.Processor.Extensions
+{
+    internal static class StringExtensions
+    {
+        public static IEnumerable<string> Slice(this string text, int size)
+        {
+            if (text == null)
+                throw new ArgumentNullException(nameof(text));
+
+            if (size <= 0)
+                throw new ArgumentException("The size of a chunk must be greater than 0.", nameof(size));
+
+            for (var i = 0; i < text.Length; i += size)
+                yield return text.Substring(i, Math.Min(size, text.Length - i));
+        }
+
+        public static string[] Split(this string text, string seperator)
+        {
+            return text.Split(new string[1] { seperator }, StringSplitOptions.None);
+        }
+    }
+}
