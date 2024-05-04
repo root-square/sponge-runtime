98f7a03c30d1f7bc56cb9a665c70b46d4c82b9dd
June <junimiso04@naver.com>
2024-05-04 18:50:01 +0900

    feat(WIP): impl converter

--- src/Sponge.Converter/Program.cs
+++ src/Sponge.Converter/Program.cs
@@ -1,10 +1,61 @@
-﻿namespace Sponge.Converter
+﻿using NetCoreServer;
+using System.Collections.Concurrent;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Sponge.Converter
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // HTTP server port
+            int port = 8080;
+            if (args.Length > 0)
+                port = int.Parse(args[0]);
+            // HTTP server content path
+            string www = "../../../../../www/api";
+            if (args.Length > 1)
+                www = args[1];
+
+            Console.WriteLine($"HTTP server port: {port}");
+            Console.WriteLine($"HTTP server static content path: {www}");
+            Console.WriteLine($"HTTP server website: http://localhost:{port}/api/index.html");
+
+            Console.WriteLine();
+
+            // Create a new HTTP server
+            var server = new Server(IPAddress.Any, port);
+            server.AddStaticContent(www, "/api");
+
+            // Start the server
+            Console.Write("Server starting...");
+            server.Start();
+            Console.WriteLine("Done!");
+
+            Console.WriteLine("Press Enter to stop the server or '!' to restart the server...");
+
+            // Perform text input
+            for (; ; )
+            {
+                string line = Console.ReadLine();
+                if (string.IsNullOrEmpty(line))
+                    break;
+
+                // Restart the server
+                if (line == "!")
+                {
+                    Console.Write("Server restarting...");
+                    server.Restart();
+                    Console.WriteLine("Done!");
+                }
+            }
+
+            // Stop the server
+            Console.Write("Server stopping...");
+            server.Stop();
+            Console.WriteLine("Done!");
         }
     }
 }
